9eecdc96c9b04d23776ac888531edd6f7b894ab0
Wesley van Schaijk <wschaijk@outlook.com>
2023-07-06 15:18:56 +0200

    Checks for size

--- data/team.js
+++ data/team.js
@@ -27,7 +27,9 @@ class Team {
      * @returns {object} The Pokémon at the given index.
      */
     index(index) {
-        return this.pokemon[index];
+        if (this.indexInTeam(index)) {
+            return this.pokemon[index];
+        }
     }
 
     /**
@@ -46,9 +48,11 @@ class Team {
      * @param {object} pokemon The Pokémon to replace the old one with.
      */
     order(oldIndex, newIndex) {
-        let pokemon = this.pokemon[oldIndex];
-        this.pokemon.splice(oldIndex, 1);
-        this.pokemon.splice(newIndex, 0, pokemon);
+        if (this.indexInTeam(oldIndex) && this.indexInTeam(newIndex)) {
+            let pokemon = this.pokemon[oldIndex];
+            this.pokemon.splice(oldIndex, 1);
+            this.pokemon.splice(newIndex, 0, pokemon);
+        }
     }
 
     /**
@@ -56,7 +60,17 @@ class Team {
      * @param {number} index The index of the Pokémon to remove.
      */
     remove (index) {
-        this.pokemon.splice(index, 1);
+        if (this.indexInTeam(index)) {
+            this.pokemon.splice(index, 1);
+        }
     }
 
+    /**
+     * Check if an index is in the team.
+     * @param {number} index the index of the Pokémon to check
+     * @returns {boolean} true if the index is in the team, false otherwise
+     */
+    indexInTeam(index) {
+        return index >= 0 && index < this.length;
+    }
 }
