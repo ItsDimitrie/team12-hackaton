f83cb1bfb74a8c6a20f8778fc994fa65d78f0606
erjbroek <broe0136@hz.nl>
2023-07-06 14:47:50 +0200

    added pokemon to navbar

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
   const homeBtn = document.getElementById("homeBtn");
   const aboutBtn = document.getElementById("aboutBtn");
   const contactBtn = document.getElementById("contactBtn");
+  const pokeBtn = document.getElementById("pokeBtn");
 
   const homeView = document.getElementById("homeView");
   const aboutView = document.getElementById("aboutView");
   const contactView = document.getElementById("contactView");
+  const pokeView = document.getElementById("pokeView");
 
   homeBtn.addEventListener("click", function() {
     showView("home");
@@ -19,10 +21,15 @@ document.addEventListener("DOMContentLoaded", function() {
     showView("contact");
   });
 
+  pokeBtn.addEventListener("click", function() {
+    showView("poke");
+  });
+
   function showView(view) {
     homeView.classList.remove("active");
     aboutView.classList.remove("active");
     contactView.classList.remove("active");
+    pokeView.classList.remove("active");
 
     if (view === "home") {
       homeView.classList.add("active");
@@ -30,6 +37,8 @@ document.addEventListener("DOMContentLoaded", function() {
       aboutView.classList.add("active");
     } else if (view === "contact") {
       contactView.classList.add("active");
+    } else if (view === "poke") {
+      pokeView.classList.add("active");
     }
   }
 });
